64ad1aa03ddbd7b828d3aa7d23fb6984fb662fac
Victor Gomes dos Santos <vgomes.sants@gmail.com>
2025-08-05 20:10:55 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/projectudemy/profnelioalves/entities/Category.java
+++ src/main/java/com/projectudemy/profnelioalves/entities/Category.java
@@ -4,12 +4,15 @@ import java.io.Serializable;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
-
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import java.util.Set;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.util.HashSet;
 
 @Entity
@@ -17,12 +20,13 @@ import java.util.HashSet;
 public class Category implements Serializable{
     private static final long serialVersionUID = 1L;
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
-    @Transient
+
+    @ManyToMany(mappedBy = "categories")
+    @JsonIgnore
     private Set<Product> products = new HashSet<>();
 
     public Category() {}
--- src/main/java/com/projectudemy/profnelioalves/entities/Product.java
+++ src/main/java/com/projectudemy/profnelioalves/entities/Product.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 
@@ -25,8 +28,8 @@ public class Product implements Serializable {
     private Double price;
     private String imgUrl;
 
-    @Transient
-    // This annotation indicates that this field is not persisted in the database
+    @ManyToMany
+    @JoinTable(name = "tb_product_categories", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name ="category_id"))
     private Set<Category> categories = new HashSet<>();
     // instanciamos para garantir que a lista não seja nula
 
